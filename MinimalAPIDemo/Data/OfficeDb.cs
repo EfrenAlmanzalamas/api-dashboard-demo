43e15b9604fb1eabc74f2251c08d5298777a01ff
Efren Almanza Lamas <efren.almanza@kingtide.com>
2022-10-25 16:13:15 -0500

    create api

---
+++ MinimalAPIDemo/Data/OfficeDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIDemo.Models;
+namespace MinimalAPIDemo.Data
+{
+    public class OfficeDb : DbContext
+    {
+        public OfficeDb(DbContextOptions<OfficeDb> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
+
